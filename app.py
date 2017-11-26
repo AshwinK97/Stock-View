23b4bcfb82963f68fb253d45afd98691c23dca36
Artemis854 <kaushal197@gmail.com>
2017-11-26 02:37:16 -0500

    Configured candlestick charts using plotly and SQL quieries

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
+#from flaskext.mysql import MySQL
 import sqlite3 as sql
 
-app = Flask(__name__)
+import json
+import plotly
 
-# https://www.quandl.com/api/v1/datasets/WIKI/AAPL.csv?column=4&sort_order=asc&collapse=quarterly&trim_start=2012-01-01&trim_end=2013-12-31
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -11,9 +16,55 @@ def home():
 	con.row_factory = sql.Row
 
 	cur = con.cursor()
-	cur.execute("select * from tickers")
+	cur.execute('''
+	select Tickers.name, Prices.date, Prices.open, Prices.close, Prices.high, Prices.low, Prices.volume
+	from Prices 
+	join Tickers on Tickers.id = Prices.ticker_id
+	where Prices.ticker_id = 1
+	order by price_id ASC
+	''')
+
+	columns = list(map(lambda col: col[0], cur.description))
 	rows = cur.fetchall();
-	return render_template("homepage.html",rows = rows)
+	
+	df = pd.DataFrame(rows)
+	df.columns = columns
+	
+	tables = [df.head(25).to_html(classes='pure-table')]
+
+	graphs = [{
+		"data": [{
+				"x": df.date.tolist(),
+				"open": df.open.tolist(),
+				"close": df.close.tolist(),
+				"high": df.high.tolist(),
+				"low": df.low.tolist(),
+				"type": 'candlestick',
+				"xaxis": 'Date',
+				"yaxis": 'Price'
+		}],
+		"layout": {
+			"title": df.name[0],
+			"dragmode": 'zoom', 
+			"margin": {
+				"r": 10, 
+				"t": 25, 
+				"b": 40, 
+				"l": 60
+			}
+		}
+	}]
+
+	# Add "ids" to each of the graphs to pass up to the client
+	# for templating
+	ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
+
+	# Convert the figures to JSON
+	# PlotlyJSONEncoder appropriately converts pandas, datetime, etc
+	# objects to their JSON equivalents
+	graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+	return render_template("homepage.html", tables = tables, ids = ids, graphJSON = graphJSON)
 
 @app.route('/about')
 def about():
