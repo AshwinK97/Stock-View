b1b3097afa274bf4568d74b63ec19cc59558bc57
Artemis854 <kaushal197@gmail.com>
2017-11-26 03:58:11 -0500

    <int:ticker> issue

--- app.py
+++ app.py
@@ -82,9 +82,10 @@ def view1():
 
 	return render_template("view1.html", tables = tables, ids = ids, graphJSON = graphJSON)
 
-@app.route('/stock/<int:ticker>')
+@app.route('/stock/<ticker>')
 def stock(ticker):
 	#ticker_info = select("select name, company from Tickers where id = ?", ticker)
+
 	price_select_all = select("select * from prices where ticker_id = ? order by price_id asc", ticker)
 
 	price_rows = price_select_all.fetchall()
